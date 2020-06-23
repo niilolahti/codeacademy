652e2a72efed69345f7f25845719127fe0924076
Niilo <dreamt85@gmail.com>
2020-06-23 09:31:42 +0300

    Backend practice from Monday 22.6. Some user information manipulation such as deletion and addition implemented and started

---
+++ client/src/containers/Users/Users.js
@@ -0,0 +1,57 @@
+import React, { useEffect, useState, useMemo, useCallback } from "react";
+import DataTable from "../../components/DataTable";
+import { FaTrash } from "react-icons/fa";
+const Users = (props) => {
+    const [users, setUsers] = useState([]);
+    useEffect(() => {
+        fetch("http://localhost:3001/users")
+            .then((response) => response.json())
+            .then((data) => setUsers(data));
+    }, []);
+    const onDelete = useCallback((userToRemove) => {
+        fetch(`http://localhost:3001/users/${userToRemove.id}`, {
+            method: "DELETE",
+        })
+            .then((response) => {
+                if (!response.ok) {
+                    console.log("Failed to remove", userToRemove);
+                } else {
+                    setUsers((users) =>
+                        users.filter((user) => user.id !== userToRemove.id)
+                    );
+                }
+            })
+            .catch((error) => console.error(error));
+    }, []);
+    const columns = useMemo(
+        () => [
+            {
+                Header: "#",
+                accessor: "id",
+            },
+            {
+                Header: "Nimi",
+                accessor: "name",
+            },
+            {
+                Header: "Sähköposti",
+                accessor: "email",
+            },
+            {
+                Header: "Toiminnot",
+                accessor: (originalRow, rowIndex) => {
+                    return <FaTrash onClick={() => onDelete(originalRow)} />;
+                },
+            },
+        ],
+        [onDelete]
+    );
+    return (
+        <div>
+            <h3>Users</h3>
+            <DataTable columns={columns} data={users} />
+        </div>
+    );
+};
+Users.displayName = "Users";
+export default Users;
